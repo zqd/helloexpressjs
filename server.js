6c6a5658192b466f53d49ff9385559bdb4080550
zqd <qiandong.zhang@bjyada.com>
2017-04-27 17:01:02 +0800

    添加压缩中间件

--- server.js
+++ server.js
@@ -9,6 +9,17 @@ const express = require('express')
 const bodyParsers = require('body-parser')
 const route = require('./routers')
 const app = express()
+const compression = require('compression')
+
+app.use(compression({filter: shouldCompress}))
+
+function shouldCompress (req, res) {
+    if (req.headers['x-no-compression']) {
+        return false
+    }
+
+    return compression.filter(req, res)
+}
 
 app.use(bodyParsers.json())
 
