d5699ded7038265f5d0fb8a8c54578fc8ea0710d
zqd <qiandong.zhang@bjyada.com>
2017-04-28 14:52:42 +0800

    增一个工具类

---
+++ pathUtil.js
@@ -0,0 +1,13 @@
+/**
+ * Author : zhangqiandong
+ * Created Date : 2017/4/28
+ * Modified By： zhangqiandong
+ * Why & What is modified  <修改原因描述>
+ * <文件描述>
+ */
+module.exports = path => params => {
+    for(const p in params){
+        path = path.replace(":"+p,params[p])
+    }
+    return path
+}
